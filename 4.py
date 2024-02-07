d7cc7e983048f5b77464621af508e81d8364395e
batyrlanmukanov <157213813+batyrlanmukanov@users.noreply.github.com>
2024-02-08 02:06:30 +0600

    function2

--- 4.py
+++ 4.py
@@ -1,22 +1,88 @@
-from math import sqrt
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def show(self):
-        print("X: ", self.x, "\t", "Y: ", self.y)
-    def move(self, x, y):
-        self.x = x
-        self.y = y
-    def dist(self, point):
-        print(sqrt( (point.x - self.x)**2 + (point.y - self.y )**2) )
+def average_imdb(movies):
+    summ = 0
+    for i in movies:
+        summ+=i.get("imdb")
+    return summ/len(movies)
 
 def main():
-    point1 = Point(1, 2)
-    point1.move(-1, -4)
-    point2 = Point(0, 1)
 
+    movies = [
+    {
+    "name": "Usual Suspects", 
+    "imdb": 7.0,
+    "category": "Thriller"
+    },
+    {
+    "name": "Hitman",
+    "imdb": 6.3,
+    "category": "Action"
+    },
+    {
+    "name": "Dark Knight",
+    "imdb": 9.0,
+    "category": "Adventure"
+    },
+    {
+    "name": "The Help",
+    "imdb": 8.0,
+    "category": "Drama"
+    },
+    {
+    "name": "The Choice",
+    "imdb": 6.2,
+    "category": "Romance"
+    },
+    {
+    "name": "Colonia",
+    "imdb": 7.4,
+    "category": "Romance"
+    },
+    {
+    "name": "Love",
+    "imdb": 6.0,
+    "category": "Romance"
+    },
+    {
+    "name": "Bride Wars",
+    "imdb": 5.4,
+    "category": "Romance"
+    },
+    {
+    "name": "AlphaJet",
+    "imdb": 3.2,
+    "category": "War"
+    },
+    {
+    "name": "Ringing Crime",
+    "imdb": 4.0,
+    "category": "Crime"
+    },
+    {
+    "name": "Joking muck",
+    "imdb": 7.2,
+    "category": "Comedy"
+    },
+    {
+    "name": "What is the name",
+    "imdb": 9.2,
+    "category": "Suspense"
+    },
+    {
+    "name": "Detective",
+    "imdb": 7.0,
+    "category": "Suspense"
+    },
+    {
+    "name": "Exam",
+    "imdb": 4.2,
+    "category": "Thriller"
+    },
+    {
+    "name": "We Two",
+    "imdb": 7.2,
+    "category": "Romance"
+    }
+    ]
 
-    point1.show()
-    point1.dist(point2)
+    print(average_imdb(movies))
 main()
